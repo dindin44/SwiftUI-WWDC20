5418e85e4613e9f265c6dc09a1dd7aab9d48d82f
robsturgeon <github@sturdysturge.com>
2020-06-27 18:36:07 +0100

    Refactored Sign in with Apple

--- Shared/SignInWithAppleExampleView.swift
+++ Shared/SignInWithAppleExampleView.swift
@@ -6,49 +6,53 @@
 //
 
 import SwiftUI
-
+import AuthenticationServices
 
 struct SignInWithAppleExampleView: View {
   @State var output = ""
+  
+  func output(_ result: Result<ASAuthorization, Error>) {
+    switch result {
+    case .success (let authResults):
+      self.output = """
+      Authorization successful
+
+      \(authResults)
+      \(authResults.provider)
+      \(authResults.credential)
+      """
+      
+    case .failure (let error):
+      self.output = "Authorization failed: \(error.localizedDescription)"
+    }
+    print(self.output)
+  }
+  
+  
   var body: some View {
     VStack {
-      Text("Tap the button to authenticate. You must be signed into an Apple ID on this device.")
+      Text("Tap the button to authenticate.\nYou must be signed into an Apple ID on this device.")
         .foregroundColor(.black)
-        .padding()
-        .padding()
+        .lineLimit(nil)
       SignInWithAppleButton(
         .signIn,
         onRequest: { request in
           request.requestedScopes = [.fullName, .email]
         },
         onCompletion: { result in
-          switch result {
-          case .success (let authResults):
-            self.output = """
-            Authorization successful
-
-            \(authResults)
-            \(authResults.provider)
-            \(authResults.credential)
-            """
-            print(self.output)
-          case .failure (let error):
-            self.output = "Authorization failed: " + error.localizedDescription
-            print(self.output)
-          }
+          output(result)
         })
         .frame(maxWidth: 375)
         .aspectRatio(7, contentMode: .fit)
-        .padding()
       Text(output)
         .foregroundColor(.black)
-        .padding()
+        
     }
+    .padding()
     .frame(maxWidth: .infinity, maxHeight: .infinity)
     .background(Color.white)
   }
 }
-
 
 struct SignInWithAppleView_Previews: PreviewProvider {
   static var previews: some View {
