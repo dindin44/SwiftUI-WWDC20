4359c51f899ef66d6385d41717ee03dffe12983d
robsturgeon <github@sturdysturge.com>
2020-06-27 18:21:50 +0100

    Removed colours that weren't necessary

--- Shared/SignInWithAppleExampleView.swift
+++ Shared/SignInWithAppleExampleView.swift
@@ -7,17 +7,6 @@
 
 import SwiftUI
 
-extension Color {
-  #if os(macOS)
-  static let systemGray = Color(NSColor.systemGray)
-  static let systemBackground =
-    Color(NSColor.windowBackgroundColor)
-  #elseif os(iOS)
-  static let systemGray = Color(UIColor.systemGray)
-  static let systemBackground =
-    Color(UIColor.systemBackground)
-  #endif
-}
 
 struct SignInWithAppleExampleView: View {
   @State var output = ""
@@ -26,43 +15,43 @@ struct SignInWithAppleExampleView: View {
       Text("Tap the button to authenticate. You must be signed into an Apple ID on this device.")
         .foregroundColor(.black)
         .padding()
-        .background(Color.systemBackground)
         .padding()
-    SignInWithAppleButton(
-      .signIn,
-      onRequest: { request in
-        request.requestedScopes = [.fullName, .email]
-      },
-      onCompletion: { result in
-        switch result {
-        case .success (let authResults):
-          self.output = """
+      SignInWithAppleButton(
+        .signIn,
+        onRequest: { request in
+          request.requestedScopes = [.fullName, .email]
+        },
+        onCompletion: { result in
+          switch result {
+          case .success (let authResults):
+            self.output = """
             Authorization successful
 
             \(authResults)
             \(authResults.provider)
             \(authResults.credential)
             """
-          print(self.output)
-        case .failure (let error):
-          self.output = "Authorization failed: " + error.localizedDescription
-          print(self.output)
-        }
-      })
-      .frame(maxWidth: 375)
-      .aspectRatio(7, contentMode: .fit)
-      .padding()
+            print(self.output)
+          case .failure (let error):
+            self.output = "Authorization failed: " + error.localizedDescription
+            print(self.output)
+          }
+        })
+        .frame(maxWidth: 375)
+        .aspectRatio(7, contentMode: .fit)
+        .padding()
       Text(output)
+        .foregroundColor(.black)
         .padding()
     }
     .frame(maxWidth: .infinity, maxHeight: .infinity)
-    .background(Color.systemGray)
+    .background(Color.white)
   }
 }
 
 
 struct SignInWithAppleView_Previews: PreviewProvider {
-    static var previews: some View {
-        SignInWithAppleExampleView()
-    }
+  static var previews: some View {
+    SignInWithAppleExampleView()
+  }
 }
