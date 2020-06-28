9d51e8a457be56a477f81f5823abf706fb3c1e91
robsturgeon <github@sturdysturge.com>
2020-06-28 11:25:32 +0100

    Added SpriteViewExampleView

---
+++ Shared/SpriteViewExampleView.swift
@@ -0,0 +1,44 @@
+//
+//  SpriteViewExampleView.swift
+//  SwiftUI-NewFeatures
+//
+//  Created by Rob Sturgeon on 28/06/2020.
+//
+
+import SwiftUI
+import SpriteKit
+
+class GameScene: SKScene {
+    override func didMove(to view: SKView) {
+        physicsBody = SKPhysicsBody(edgeLoopFrom: frame)
+    }
+
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first else { return }
+        let location = touch.location(in: self)
+        let box = SKSpriteNode(color: UIColor.red, size: CGSize(width: 50, height: 50))
+        box.position = location
+        box.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: 50, height: 50))
+        addChild(box)
+    }
+}
+struct SpriteViewExampleView: View {
+    var scene: SKScene {
+        let scene = GameScene()
+        scene.size = CGSize(width: 300, height: 400)
+        scene.scaleMode = .fill
+        return scene
+    }
+
+    var body: some View {
+      SpriteView(scene: scene)
+          .frame(width: 300, height: 400)
+          .edgesIgnoringSafeArea(.all)
+    }
+}
+
+struct SpriteViewExampleView_Previews: PreviewProvider {
+    static var previews: some View {
+        SpriteViewExampleView()
+    }
+}
