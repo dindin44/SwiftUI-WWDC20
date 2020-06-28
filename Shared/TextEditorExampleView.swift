7c552978a4819d43842d449780ace80aea698a2f
robsturgeon <github@sturdysturge.com>
2020-06-28 06:19:09 +0100

    Added improved colour picker to TextEditorExampleView

--- Shared/TextEditorExampleView.swift
+++ Shared/TextEditorExampleView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 enum WeightType: String, CaseIterable {
   case black, bold, heavy, light, medium, regular, semibold, thin, ultraLight
-
+  
   
   var weight: Font.Weight {
     switch self {
@@ -42,38 +42,73 @@ struct TextEditorExampleView: View {
   @State private var fontSize = CGFloat(12)
   @State private var customFont = true
   @State private var foregroundColor = Color.primary
+  @State private var colourPickerShown = true
   var body: some View {
     ScrollView(.vertical) {
-    VStack {
-      Picker(selection: $fontWeight, label: EmptyView()) {
-        ForEach(WeightType.allCases, id: \.self) { fontWeight in
-          Text(fontWeight.rawValue)
+      VStack {
+        Picker(selection: $fontWeight, label: EmptyView()) {
+          ForEach(WeightType.allCases, id: \.self) { fontWeight in
+            Text(fontWeight.rawValue)
+          }
         }
+        .frame(height: 130)
+        .offset(y: -25)
+        
+        Stepper(value: $fontSize, in: 1...100) {
+          Text("Font size: \(String(format: "%.1f", fontSize))")
+        }
+        HStack {
+          Button("Clear text") {
+            self.text = ""
+          }
+          ColourPickerButton(dismissSheet: true, buttonTitle: "Text colour", previousColour: foregroundColor, colour: $foregroundColor, colourPickerShown: $colourPickerShown)
+          
+        }
+        TextEditor(text: $text)
+          .font(.system(size: fontSize, weight: self.fontWeight.weight))
+          .foregroundColor(foregroundColor)
+          .frame(height: 300)
       }
-      .frame(height: 130)
-      .offset(y: -25)
-      
-      Stepper(value: $fontSize, in: 1...100) {
-        Text("Font size: \(String(format: "%.1f", fontSize))")
-      }
-      HStack {
-      Button("Clear text") {
-        self.text = ""
-      }
-        ColorPicker(".foregroundColor", selection: $foregroundColor)
-      }
-      TextEditor(text: $text)
-        .font(.system(size: fontSize, weight: self.fontWeight.weight))
-        .foregroundColor(foregroundColor)
-        .frame(height: 300)
-    }
-    .padding(.horizontal)
+      .padding(.horizontal)
     }
   }
 }
 
-struct TextEditorExampleView_Previews: PreviewProvider {
-  static var previews: some View {
-    TextEditorExampleView()
+
+struct ColourPickerButton: View {
+  let dismissSheet: Bool
+  let buttonTitle: String
+  let previousColour: Color
+  @Binding var colour: Color
+  @State var colourInitialised = false
+  @Binding var colourPickerShown: Bool
+  var body: some View {
+    Button(buttonTitle) {
+      colourPickerShown.toggle()
+    }
+    .padding()
+    .background(Color.white)
+    .cornerRadius(5)
+    .onChange(of: colour) { _ in
+      if dismissSheet && colour == previousColour {
+        self.colourPickerShown.toggle()
+      }
+    }
+    .sheet(isPresented: $colourPickerShown) {
+      ColourPickerSheetView(colour: $colour, previousColour: colour)
+    }
+  }
+}
+
+struct ColourPickerSheetView: View {
+  @Binding var colour: Color
+  let previousColour: Color
+  var body: some View {
+    VStack {
+      if colour == previousColour {
+        Text("Swipe down to dismiss")
+        ColorPicker("Colour", selection: $colour)
+      }
+    }
   }
 }
